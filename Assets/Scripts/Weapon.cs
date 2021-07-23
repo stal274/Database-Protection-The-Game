96f7d7f1657e06039fcc4c3d4ac47af5344225a9
Павел <plyushhaj2@gmail.com>
2021-07-23 08:43:03 +0300

    Add scripts for shield and weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]private int _damage;
+    [SerializeField]private AudioSource _punchSound;
+
+    void DealDamage(){
+        
+
+    }
+  
+}
