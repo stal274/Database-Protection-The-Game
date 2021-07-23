af5733ecf1b112de25d54600e8f744f98721c73e
Павел <plyushhaj2@gmail.com>
2021-07-23 20:26:58 +0300

    Add ability to absorb damage

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _armor;
+    [SerializeField] private string[] _vulnerableTo, _protectedFrom;
+    [SerializeField] private Dictionary<string, double> _damageCoefficient;
+
+    private void Start()
     {
-        
+        _damageCoefficient = new Dictionary<string, double>{
+            {"Increased",1.5},
+            {"Normal",1.0},
+            {"Reduced",0.3}
+            };
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void ReceiveDamage(Weapon weapon)
     {
-        
+        string typeOfWeapon = "Normal";
+        _armor = Convert.ToInt32(_armor - weapon.Damage * _damageCoefficient[typeOfWeapon]);
     }
+
+
 }
