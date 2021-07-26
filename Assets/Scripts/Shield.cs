61897360117e5f8fc5ae87ef451ba6da51352e0d
stal274 <plyushhaj2@gmail.com>
2021-07-26 09:13:42 +0300

    add shield physic

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -5,25 +5,36 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    [Header("Настройка щита")]
     [SerializeField] private int _armor;
-    [SerializeField] private string[] _vulnerableTo, _protectedFrom;
-    [SerializeField] private Dictionary<string, double> _damageCoefficient;
+    [SerializeField] private double _increasedCoefficient;
+    [SerializeField] private Weapon[] _vulnerableTo;
+    [SerializeField] private double _reducedCoefficient;
+    [SerializeField] private Weapon[] _protectedFrom;
+    private readonly Dictionary<Weapon, double> _damageCoefficient = new Dictionary<Weapon, double>();
 
     private void Start()
     {
-        _damageCoefficient = new Dictionary<string, double>{
-            {"Increased",1.5},
-            {"Normal",1.0},
-            {"Reduced",0.3}
-            };
+        foreach (Weapon w in _vulnerableTo)
+        {
+            _damageCoefficient[w] = _increasedCoefficient;
+        }
+        foreach (Weapon w in _protectedFrom)
+        {
+            _damageCoefficient[w] = _reducedCoefficient;
+        }
     }
 
 
     private void ReceiveDamage(Weapon weapon)
     {
-        string typeOfWeapon = "Normal";
-        _armor = Convert.ToInt32(_armor - weapon.Damage * _damageCoefficient[typeOfWeapon]);
+        _armor = Convert.ToInt32(_armor - weapon.Damage * _damageCoefficient[weapon]);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(other.gameObject);
     }
 
-
 }
+
+
