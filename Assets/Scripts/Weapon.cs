94ff9aede3ab0cb5ffc623ea54bcd4efb5b6a70c
stal274 <plyushhaj2@gmail.com>
2021-07-26 10:06:26 +0300

    Project update

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] private string _typeOfCyberattack;
     [SerializeField] private int _damage;
     [SerializeField] private AudioSource _punchSound;
+    [SerializeField] private Rigidbody2D _bullet;
 
     public int Damage { get => _damage; }
-    public string TypeOfCyberattack { get => _typeOfCyberattack; }
-
+   
     public void ToDamage()
     {
 
 
     }
-
 }
