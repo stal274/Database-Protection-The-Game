d3306290d2aa86cbc9467cacd92de7823776b8b3
stal274 <plyushhaj2@gmail.com>
2021-07-26 10:05:46 +0300

    Add bullet class

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _collider;
+    
+}
