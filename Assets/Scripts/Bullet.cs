ac56e93077725d3dc5ed5a16902f2c5cb165c54a
Павел <plyushhaj2@gmail.com>
2021-07-30 09:48:05 +0300

    Add physic for bullet prefab

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _collider;
-    
+    [SerializeField] private Color32 _color;
+    [SerializeField ] private float _velocity;
+    private void Start()
+    {
+        _collider.velocity = new Vector2(-_velocity, 0);
+    }
+
 }
