c2821bdc466fe7c8fa6dcf789bc5ff042a003271
Павел <plyushhaj2@gmail.com>
2021-07-30 09:48:58 +0300

    Add reloading method

--- Assets/Scripts/Magazine.cs
+++ Assets/Scripts/Magazine.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Magazine : MonoBehaviour
 {
-    
+
     [SerializeField] private Bullet[] _bullets;
-
-    public void Reload(){
-
+    private int _bulletsCount;
+    private void Start()
+    {
+        _bulletsCount = _bullets.Length;
+    }
+    public void SendAPatron( Weapon weapon)
+    {
+        Bullet bulletToLoad = _bullets[_bulletsCount];
+        if (bulletToLoad == null) return;
+        weapon.Load(bulletToLoad);
+        _bulletsCount -= 1;
     }
 
 }
